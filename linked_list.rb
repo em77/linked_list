b9e1dbf6afdd9910d84dfa5c85c807a75e7c5ab4
Eric M <ericmarchese77@gmail.com>
2016-09-14 14:11:52 -0400

    Added append, prepend methods

--- linked_list.rb
+++ linked_list.rb
@@ -1,12 +1,32 @@
 class LinkedList
   attr_reader :head, :tail
   def initialize
-    @tail = node_factory("tail")
-    @head = node_factory("head", tail)
+    @head = nil
+    @tail = nil
   end
 
   def node_factory(value = nil, next_node = nil)
     Node.new(value, next_node)
+  end
+
+  def prepend(node)
+    if @head.nil?
+      @head = node
+      @tail = node
+    else
+      node.next_node = @head
+      @head = node
+    end
+  end
+
+  def append(node)
+    if @head.nil?
+      @head = node
+      @tail = node
+    else
+      @tail.next_node = node
+      @tail = node
+    end
   end
 end
 
